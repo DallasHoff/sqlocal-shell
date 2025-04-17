3abb8349b1107019244813c124fe507ad8ca25a4
Dallas Hoffman <dallashoff@gmail.com>
2025-04-17 17:35:18 +0200

    Add prettier; move app config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SqlocalShell
+# SQLocal Shell
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.8.
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
+};
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => {
+  console.error(err);
+});
