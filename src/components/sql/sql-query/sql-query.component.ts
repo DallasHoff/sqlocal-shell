8cdf860caab771254002498315f509d1aec2360c
Dallas Hoffman <dallashoff@gmail.com>
2025-04-29 23:50:31 -0400

    Suggestion alignment fix

--- src/components/sql/sql-query/sql-query.component.ts
+++ src/components/sql/sql-query/sql-query.component.ts
@@ -5,6 +5,7 @@ import { highlight } from 'sql-highlight';
   selector: 'sql-query',
   imports: [],
   templateUrl: './sql-query.component.html',
+  styleUrl: './sql-query.component.scss',
 })
 export class SqlQueryComponent {
   sql = input<string>('');
